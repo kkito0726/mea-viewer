64629cbf79fadc126289a258ac507e6093475ab3
kkito <k.kito0726@gmail.com>
2024-06-02 19:15:03 +0900

    dockerコンテナ内で動くように修正

--- server/s3.py
+++ server/s3.py
@@ -1,5 +1,5 @@
 from minio import Minio
 
 minio_client = Minio(
-    "localhost:9000", access_key="minio_admin", secret_key="minio_pass", secure=False
+    "minio:9000", access_key="minio_admin", secret_key="minio_pass", secure=False
 )
