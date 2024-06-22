34c52d2faec944535137f32837b50623cf76e5db
kkito <k.kito0726@gmail.com>
2024-06-22 23:07:05 +0900

    spring boot追加

---
+++ figure-management/src/main/java/com/mea_viewer_backend/figure_management/FigureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mea_viewer_backend.figure_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigureManagementApplication.class, args);
+	}
+
+}
---
+++ figure-management/src/test/java/com/mea_viewer_backend/figure_management/FigureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mea_viewer_backend.figure_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigureManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
