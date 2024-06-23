ecac7567d3ea13a967ad7b6707fd59166d32ff5e
kkito <k.kito0726@gmail.com>
2024-06-23 16:28:20 +0900

    generateJooqプロジェクトの追加

--- backend/figure-management/src/main/java/com/mea_viewer_backend/figure_management/FigureManagementApplication.java
+++ backend/figure-management/src/main/java/com/mea_viewer_backend/figure_management/FigureManagementApplication.java
@@ -1,13 +1,19 @@
 package com.mea_viewer_backend.figure_management;
 
+import com.jooq.generated.tables.Draw2dImage;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FigureManagementApplication {
+	@Autowired DSLContext dslContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FigureManagementApplication.class, args);
+
+
 	}
 
 }
