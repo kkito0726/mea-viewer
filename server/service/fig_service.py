8943056d751adc40833dd49b4e0f2fc47fe531c1
kkito <k.kito0726@gmail.com>
2025-06-07 01:29:20 +0900

    受け取った電極データ以外はダミーデータで保管してpyMEAで使用できるようにする

--- server/service/fig_service.py
+++ server/service/fig_service.py
@@ -1,16 +1,17 @@
 import io
 import json
 
+import matplotlib
+
+matplotlib.use("Agg")  # GUIバックエンドを使用しないように設定
+
 import numpy as np
-import pyMEA
 from flask import request
-from pyMEA import FigMEA
+from pyMEA import FigMEA, detect_peak_all, detect_peak_neg, detect_peak_pos
 from pyMEA.core.Electrode import Electrode
 from pyMEA.read.model.HedPath import HedPath
 from pyMEA.read.model.MEA import MEA
 
-from lib.peak_detection import detect_peak_neg, detect_peak_pos
-from lib.plot import plotPeaks, raster_plot, showDetection
 from model.form_value import FormValue
 from model.peak_form_value import PeakFormValue
 
@@ -30,10 +31,30 @@ def decode_request():
 
 
 def clean_data(data):
+    if not np.isnan(data).any():
+        return data
+
     volt = np.array([row[~np.isnan(row)] for row in data[1:]])
     t = data[0][: len(volt[0])]
     t = t.reshape(1, len(t))
     return np.append(t, volt, axis=0)
+
+
+# 受け取った電極データ以外はダミーデータで保管してpyMEAで使用できるようにする
+def complete_data(data, form_value: FormValue):
+    if len(data) == 65:
+        return data
+
+    length = len(data[0])
+    result = np.ones((65, length), dtype=data[0].dtype)
+    result[0] = data[0]
+
+    index = 1
+    for ch in range(1, 65):
+        if ch in set(form_value.chs):
+            result[ch] = data[index]
+            index += 1
+    return result
 
 
 def showAllService() -> tuple[io.BytesIO, str]:
@@ -79,8 +100,17 @@ def showSingleService() -> tuple[list[int], list[io.BytesIO], str]:
 def showDetectionService() -> tuple[io.BytesIO, str]:
     data, json_data = decode_request()
     form_value = FormValue(json_data=json_data)
+    data = complete_data(data, form_value)
+    fm = create_figMEA(data, form_value)
 
-    image_buf = showDetection(data, form_value, form_value.chs)
+    image_buf = fm.showDetection(
+        form_value.chs,
+        start=form_value.start,
+        end=form_value.end,
+        figsize=(form_value.x_ratio, form_value.y_ratio),
+        dpi=form_value.dpi,
+        isBuf=True,
+    )
 
     return image_buf, form_value.filename
 
@@ -90,29 +120,19 @@ def rasterPlotService() -> tuple[io.BytesIO, str]:
     form_value = FormValue(json_data=json_data)
     peak_form_value = PeakFormValue(json_data=json_data)
 
-    isPos, isNeg = peak_form_value.isPositive, peak_form_value.isNegative
+    data = complete_data(data, form_value)
+    fm = create_figMEA(data, form_value)
+    peak_index = get_peak_indexes(peak_form_value, fm.data)
 
-    if not isPos and not isNeg:
-        return io.BytesIO(), ""
-
-    if not isPos and isNeg:
-        peak_index = detect_peak_neg(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
-        image_buf = raster_plot(data, form_value, form_value.chs, peak_index)
-    elif isPos and not isNeg:
-        peak_index = detect_peak_pos(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
-        image_buf = raster_plot(data, form_value, form_value.chs, peak_index)
-    else:
-        pos_peak = detect_peak_pos(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
-        neg_peak = detect_peak_neg(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
-        image_buf = raster_plot(data, form_value, form_value.chs, pos_peak, neg_peak)
+    image_buf = fm.raster_plot(
+        peak_index,
+        form_value.chs,
+        figsize=(form_value.x_ratio, form_value.y_ratio),
+        start=form_value.start,
+        end=form_value.end,
+        dpi=form_value.dpi,
+        isBuf=True,
+    )
 
     return image_buf, form_value.filename
 
@@ -123,7 +143,7 @@ def draw_2d_service() -> tuple[list[io.BytesIO], str]:
     peak_form_value = PeakFormValue(json_data=json_data)
 
     fm = create_figMEA(data, form_value)
-    peak_index = pyMEA.detect_peak_neg(
+    peak_index = detect_peak_neg(
         fm.data, peak_form_value.distance, peak_form_value.threshold
     )
     image_buf_list = fm.draw_2d(peak_index, dpi=form_value.dpi, isBuf=True)
@@ -137,7 +157,7 @@ def draw_3d_service() -> tuple[list[io.BytesIO], str]:
     peak_form_value = PeakFormValue(json_data)
 
     fm = create_figMEA(data, form_value)
-    peak_index = pyMEA.detect_peak_neg(
+    peak_index = detect_peak_neg(
         fm.data, peak_form_value.distance, peak_form_value.threshold
     )
     image_bufs = fm.draw_3d(peak_index, dpi=form_value.dpi, isBuf=True)
@@ -150,24 +170,45 @@ def plot_peaks_service():
     peak_form_value = PeakFormValue(json_data=json_data)
     form_value = FormValue(json_data=json_data)
 
-    neg_peak_index, pos_peak_index = [], []
-
-    if peak_form_value.isNegative:
-        neg_peak_index = detect_peak_neg(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
-
-    if peak_form_value.isPositive:
-        pos_peak_index = detect_peak_pos(
-            data, peak_form_value.distance, peak_form_value.threshold
-        )
+    data = complete_data(data, form_value)
+    fm = create_figMEA(data, form_value)
+    peak_index = get_peak_indexes(peak_form_value, fm.data)
 
     image_bufs = [
-        plotPeaks(data, form_value, i, neg_peak_index, pos_peak_index)
-        for i in range(1, len(data))
+        fm.plotPeaks(
+            ch,
+            peak_index,
+            start=form_value.start,
+            end=form_value.end,
+            volt_min=form_value.volt_min,
+            volt_max=form_value.volt_max,
+            figsize=(form_value.x_ratio, form_value.y_ratio),
+            dpi=form_value.dpi,
+            isBuf=True,
+        )
+        for ch in form_value.chs
     ]
 
     return form_value.chs, image_bufs, form_value.filename
+
+
+def get_peak_indexes(peak_form_value: PeakFormValue, data: MEA):
+    if not peak_form_value.isNegative and not peak_form_value.isPositive:
+        raise ValueError("ピーク抽出条件が指定されていません")
+    elif peak_form_value.isNegative and peak_form_value.isPositive:
+        return detect_peak_all(
+            data,
+            (peak_form_value.threshold, peak_form_value.threshold),
+            peak_form_value.distance,
+        )
+    elif peak_form_value.isNegative:
+        return detect_peak_neg(
+            data, peak_form_value.distance, peak_form_value.threshold
+        )
+    else:
+        return detect_peak_pos(
+            data, peak_form_value.distance, peak_form_value.threshold
+        )
 
 
 def create_figMEA(data, form_value: FormValue):
